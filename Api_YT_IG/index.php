ac256e84e049fc94f1284f53731f1dcc0d8a7c23
GTM-Winter <dimastriana03@gmail.com>
2022-05-26 12:54:37 +0700

    menggabungkan jadi 1 folder

---
+++ Api_YT_IG/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+
+  <title>DimsMovies</title>
+</head>
+<body>
+  <h1 class="text-center">Hello Word</h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col text-center">
+        <h4>Sosial Media</h4>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        
+      </div>
+    </div>
+  </div>
+  
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+</body>
+</html>

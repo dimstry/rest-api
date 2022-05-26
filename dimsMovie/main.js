23f4937c7938e704c364eda420fe0757d5192b5d
GTM-Winter <dimastriana03@gmail.com>
2022-05-26 12:38:45 +0700

    finish dimsMovie

--- dimsMovie/main.js
+++ dimsMovie/main.js
@@ -20,7 +20,7 @@ function CariMovie() {
             <div class="card-body">
             <h5 class="card-title">`+ data.Title +`</h5>
             <h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h6>
-            <a href="#" class="card-link" toggle="modal" data-target="#exampleModal">Detail</a>
+            <a href="#" class="card-link detail" data-toggle="modal" data-target="#exampleModal" data-id="`+data.imdbID +`">Detail</a>
             </div>
             </div>
             </div>
@@ -40,11 +40,49 @@ function CariMovie() {
   });
 }
 
+
 $('#search-button').on('click', function() {
   CariMovie();
 });
+
+
 $('#search-input').on('keyup', function(e) {
   if (e.keycode === 13) {
     CariMovie();
   }
+});
+
+
+$('#movie-list').on('click', '.detail', function() {
+  $.ajax({
+    url: 'http://www.omdbapi.com/',
+    type: 'get',
+    dataType: 'json',
+    data: {
+      'apikey': 'c9bc65ca',
+      'i': $(this).data('id')
+    },
+    success: function(result) {
+      if (result.Response == "True") {
+        $('.modal-body').html(`
+          <div class="container-fluid">
+          <div class="row">
+          <div class="col-md-4">
+          <img src="`+result.Poster+`" class="img-fluid">
+          </div>
+          <div class="col-md-8">
+          <ul class="list-group">
+          <li class="list-group-item"><h3>`+result.Title+`</h3></li>
+          <li class="list-group-item">Release : `+result.Released+`</li>
+          <li class="list-group-item">Genre : `+result.Genre+`</li>
+          <li class="list-group-item">Director : `+result.Director+`</li>
+          <li class="list-group-item"> Actors : `+result.Actors+`</li>
+          </ul>
+          </div>
+          </div>
+          </div>
+          `);
+      }
+    }
+  });
 });

5a18d718752560ff0ad613513d25e81e3359d916
GTM-Winter <dimastriana03@gmail.com>
2022-05-26 16:55:07 +0700

    finish yt

--- Api_YT_IG/index.php
+++ Api_YT_IG/index.php
@@ -1,20 +1,30 @@
 <?php 
-  $ch=curl_init();
-  curl_setopt($ch, CURLOPT_URL,'https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCzgxx_DM2Dcb9Y1spb9mUJA&key=AIzaSyCrbQQ-XkhDWjiOmcSD7KlyCVS6ZaFQRI8');
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  $response = curl_exec($ch);
-  $result = json_decode($response, true);
-  var_dump($result);
-  if(curl_exec($ch) === false)
-  {
-      echo "Curl error: " . curl_error($ch);
+
+  function get_curl($url) {
+    $ch=curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($response, true);
   }
-  else
+  
+  $result=get_curl('https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCzgxx_DM2Dcb9Y1spb9mUJA&key=AIzaSyCrbQQ-XkhDWjiOmcSD7KlyCVS6ZaFQRI8'); 
+  $youtubePP=$result['items'][0]['snippet']['thumbnails']['medium']['url'];
+  $ChanelName=$result['items'][0]['snippet']['title'];
+  $Subriber=$result['items'][0]['statistics']['subscriberCount'];
+  function rupiah($angka)
   {
-      echo "Operation completed without any errors";
+      return number_format($angka,0,',','.');
   }
-  curl_close($ch);
-  ?>
+  
+  // latest video
+  $urlVidio="https://www.googleapis.com/youtube/v3/search?key=AIzaSyCrbQQ-XkhDWjiOmcSD7KlyCVS6ZaFQRI8&channelId=UCzgxx_DM2Dcb9Y1spb9mUJA&maxResults=1&order=date&part=snippet";
+  $hasil=get_curl($urlVidio);
+  $vidioLast=$hasil['items'][0]['id']['videoId'];
+?>
 
 <!doctype html>
 <html lang="en">
@@ -40,17 +50,18 @@
       <div class="col-md-6">
         <div class="row">
           <div class="col-md-4">
-            <img src="..." alt="" width="100px" class="rounded-circle thumbnail">
+            <img src="<?= $youtubePP ?>" alt="" width="100px" class="rounded-circle thumbnail">
           </div>
-          <div class="col-md-8">
-            <h6>Twice</h6>
-            <p>7 juta subscribe</p>
+          <div class="col-md-8 mt-2">
+            <h6><?= $ChanelName ?></h6>
+            <p><?= rupiah($Subriber);?> Subscribers</p>
+            <div class="g-ytsubscribe" data-channelid="UCzgxx_DM2Dcb9Y1spb9mUJA" data-layout="default" data-count="default"></div>
           </div>
         </div>
         <div class="row">
           <div class="col">
             <div class="embed-responsive embed-responsive-16by9">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/vPwaXytZcgI?rel=0" allowfullscreen></iframe>
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $vidioLast ?>?rel=0"></iframe>
             </div>
           </div>
         </div>
@@ -70,6 +81,7 @@
   </div>
   
   <!-- Optional JavaScript -->
+ <script src="https://apis.google.com/js/platform.js"></script>
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>

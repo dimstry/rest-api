f59bfdb5537f373d237fff37d12b29dd1fc5dd37
GTM-Winter <dimastriana03@gmail.com>
2022-05-26 10:06:02 +0700

    add new studi kasus

---
+++ dimsMovie/main.js
@@ -0,0 +1,50 @@
+function CariMovie() {
+  $('#movie-list').html('');
+  $.ajax({
+    url: 'http://www.omdbapi.com/',
+    type: 'get',
+    dataType: 'json',
+    data: {
+      'apikey': 'c9bc65ca',
+      's': $('#search-input').val()
+    },
+    success: function(result) {
+      if (result.Response == "True") {
+        let movie = result.Search;
+
+        $.each(movie, function(i, data) {
+          $('#movie-list').append(`
+            <div class="col-md-4 col-sm-12">
+            <div class="card mb-3">
+            <img class="card-img-top" src="`+ data.Poster +`" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">`+ data.Title +`</h5>
+            <h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h6>
+            <a href="#" class="card-link" toggle="modal" data-target="#exampleModal">Detail</a>
+            </div>
+            </div>
+            </div>
+            `)
+        });
+        $('#search-input').val('');
+      } else {
+        $('#movie-list').html(
+          `
+          <div class="col">
+          <h1 class="text-center">Movie not found</h1>
+          </div>
+          `)
+      }
+      $('#search-input').val('');
+    }
+  });
+}
+
+$('#search-button').on('click', function() {
+  CariMovie();
+});
+$('#search-input').on('keyup', function(e) {
+  if (e.keycode === 13) {
+    CariMovie();
+  }
+});
